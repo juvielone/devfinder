00744bd99573d84cb1fa2ac47dc09b0d12a1e95b
Juvielone Lagos <62714904+juvielone@users.noreply.github.com>
2022-07-03 20:53:50 +0800

    Updated profiles in About section

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Home />} />
 
-            <Route exact path="/about" element={<About />} />
+            {/* <Route exact path="/about" element={<About />} /> */}
 
             <Route exact path="/user/:nameLogin" element={<OneUser />} />
 
